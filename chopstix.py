6253bc1fe61e128bef6208d2860908449f98118f
Aayush Singh <aayushkt@gmail.com>
2022-12-05 11:38:37 -0500

    big fixes and refactoring for GUI update

--- chopstix.py
+++ chopstix.py
@@ -1,77 +1,91 @@
 import numpy as np
-from collections import OrderedDict
 
-class Graph:
+class ChopstixBot:
     parents = []
     children = []
-    vertexCount = 0
+    stateCount = 0
     numOfFingers = 0
     handSet = []
+    switchingAllowed = True
 
-    def __init__(self, numOfFingers):
+    def __init__(self, numOfFingers, switchingAllowed):
         self.numOfFingers = numOfFingers
+        self.switchingAllowed = switchingAllowed
         self.handSet = self.generateHandSet(numOfFingers)
-        self.vertexCount = len(self.handSet) * len(self.handSet) * 2
-        self.vertexCount = self.vertexCount
-        for _ in range(0, self.vertexCount):
+        self.stateCount = len(self.handSet) * len(self.handSet) * 2
+        self.stateCount = self.stateCount
+        for _ in range(0, self.stateCount):
             self.parents.append([])
             self.children.append([])
         self.initializeGraphEdges()
 
-    # returns a vector of length vertexCount where the nth
-    # element is 0.5 iff the nth stateIndex results
-    # in a loop that is impossible to exit (the game
-    # can never end). All other entries are 0
-    def getSolutionsForUnreachableStates(self):
-        endIsReachable = [0.5] * self.vertexCount
+    # returns a vector of length stateCount where the nth
+    # element is 0.5 iff the nth stateIndex is indeterminate.
+    # All other entries are 0
+    # A state is indeterminate iff for all states that could
+    # be reached from that state, none result in a player winning
+    # (Often occurs if switching is not allowed, and is usually just
+    # two players stuck in an infinite loop with no other options)
+    def getSolutionsForIndeterminateStates(self):
+        determinateStates = [0.5] * self.stateCount
         checkIfParentsAreReachable = []
-        for stateIndex in range(0, self.vertexCount):
+        for stateIndex in range(0, self.stateCount):
             temp = self.gameOverStatus(stateIndex)
             if temp == 0 or temp == 1:
                 checkIfParentsAreReachable.append(stateIndex)
-                endIsReachable[stateIndex] = 0
+                determinateStates[stateIndex] = 0
         while(len(checkIfParentsAreReachable)):
             currState = checkIfParentsAreReachable.pop()
-            endIsReachable[currState] = 0
+            determinateStates[currState] = 0
             for parent in self.parents[currState]:
-                if endIsReachable[parent]:
+                if determinateStates[parent]:
                     checkIfParentsAreReachable.append(parent)
-        return endIsReachable
+        return determinateStates
 
-    # Returns a vector of length vertexCount where the nth
+    # Returns a vector of length stateCount where the nth
     # element is: a 0 iff player 0 can guaranteed win from the
     # nth state, a 1 iff player 1 can guaranteed win from the nth
     # state, -1 otherwise
     def getSolutionsForPerfectPlay(self):
-        solution = [-1] * self.vertexCount
+        # All states start out as neither a win or loss
+        solution = [-1] * self.stateCount
+        # States we need to evaluate later will be stored here
         statesToEvaluate = []
-        for stateIndex in range(0, self.vertexCount):
+        # For each state, if it is a win/loss, store all parents 
+        # of that state to be a potential guarantee win/loss
+        for stateIndex in range(0, self.stateCount):
             temp = self.gameOverStatus(stateIndex)
             if temp == 0 or temp == 1:
                 for parent in self.parents[stateIndex]:
                     statesToEvaluate.append(parent)
                 solution[stateIndex] = temp
+        # Now we iterate through the rest of the graph
+        # from the 'leaves' (game over states) upward
         while(len(statesToEvaluate)):
+            # For each state we need to evaluate,
             currState = statesToEvaluate.pop()
-            if solution[currState] != -1: continue
-            for child in self.children[currState]:
-                if solution[child] == self.stateIndexToState(currState)[2]:
-                    solution[currState] = solution[child]
-                    for parent in self.parents[currState]:
-                        statesToEvaluate.append(parent)
-                        break
-            allChildrenMatch = True
-            for child in self.children[currState]:
-                allChildrenMatch = (solution[child] == solution[self.children[currState][0]] and solution[child] != -1)
-            if allChildrenMatch: 
-                solution[currState] = solution[self.children[currState][0]]
-                for parent in self.parents[currState]:
-                    statesToEvaluate.append(parent)
+            # If it hasn't been solved yet
+            if solution[currState] == -1: 
+                # Check to see if the player whose turn it is can secure a win
+                for child in self.children[currState]:
+                    if solution[child] == self.stateIndexToState(currState)[2]:
+                        solution[currState] = solution[child]
+                        for parent in self.parents[currState]:
+                            statesToEvaluate.append(parent)
+                # Otherwise its a guaranteed loss if all children are a loss
+                if solution[currState] == -1:
+                    allChildrenMatch = True
+                    for child in self.children[currState]:
+                        allChildrenMatch = allChildrenMatch and (solution[child] == solution[self.children[currState][0]] and solution[child] != -1)
+                    if allChildrenMatch: 
+                        solution[currState] = solution[self.children[currState][0]]
+                        for parent in self.parents[currState]:
+                            statesToEvaluate.append(parent)
         return solution
 
     # Prints the entire graph structure. Used for debugging
     def printGraph(self):
-        for stateIndex in range(0, self.vertexCount):
+        for stateIndex in range(0, self.stateCount):
             state = self.stateIndexToState(stateIndex)
             if len(self.children[stateIndex]) == 0:
                 print(f"{state}: has no children;")
@@ -84,7 +98,7 @@ class Graph:
     # Prints the parents of every state in the graph.
     # Used for debugging
     def printAllParents(self):
-        for stateIndex in range(0, self.vertexCount):
+        for stateIndex in range(0, self.stateCount):
             if len(self.parents[stateIndex]) == 0:
                 print(f"{stateIndex}: has no parents;")
             else:
@@ -93,7 +107,7 @@ class Graph:
     # Gets the children of each state via getChildrenOfState()
     # and then accordingly updates parents[] and children[]
     def initializeGraphEdges(self):
-        for stateIndex in range(0, self.vertexCount):
+        for stateIndex in range(0, self.stateCount):
             childrenStates = self.getChildrenOfState(self.stateIndexToState(stateIndex))
             for state in childrenStates:
                 childIndex = self.stateToStateIndex(state)
@@ -106,9 +120,9 @@ class Graph:
         if state[0] == (0, 0) and state[1] == (0, 0):
             return 0.5
         elif state[0] == (0, 0):
-            return 0
-        elif state[1] == (0, 0):
             return 1
+        elif state[1] == (0, 0):
+            return 0
         else:
             return -1
 
@@ -150,9 +164,10 @@ class Graph:
         tapStates = self.getAllTapStates(parentState)
         for state in tapStates:
             childStates.append(state)
-        switchStates = self.getAllSwitchStates(parentState)
-        for state in switchStates:
-            childStates.append(state)
+        if self.switchingAllowed:
+            switchStates = self.getAllSwitchStates(parentState)
+            for state in switchStates:
+                childStates.append(state)
         childStates = self.__removeDuplicates__(childStates)
         return childStates
 
@@ -237,31 +252,39 @@ class Graph:
     # state on how likely it is to win
     # for each player from a given point
     # INPUT: (optional) perfectPlaySolutions vector as generated by self.getSolutionsForPerfectPlay
-    #        (optional) unreachableStates vector as generated by self.getSolutionsForUnreachableStates
-    def solveForStateRankings(self, perfectPlaySolutions = None, unreachableStates = None):
+    #        (optional) indeterminateStates vector as generated by self.getSolutionsForIndeterminateStates
+    def solveForStateRankings(self, perfectPlaySolutions = None, indeterminateStates = None):
         # If a perfect play vector is not given, we assume players will simply choose their next move 
         # randomly (without evaluating which moves result in guaranteed wins). However, each end state
-        # must still correspond with a value of 1 or 0 (depending on who wins). 
+        # must still correspond with a value of 1 or 0 (depending on who wins).
+        # 
+        # This means each state's ranking will simply be the average of it's childrens ranks.
+        #  
         # This would correspond to a perfectPlay vector of all -1's, except for the end game states with
         # values of 0 or 1 to determine which player wins in the end. See 
         # self.getSolutionsForPerfectPlay for more information
         if perfectPlaySolutions == None:
-            perfectPlaySolutions = [-1] * self.vertexCount
-        # If an unreachable state vector is not given, we assume that all game states can eventually
+            perfectPlaySolutions = [-1] * self.stateCount
+            for stateIndex in range(self.stateCount):
+                temp = self.gameOverStatus(stateIndex)
+                if(temp == 0 or temp == 1):
+                    perfectPlaySolutions[stateIndex] = temp
+
+        # If an indeterminate state vector is not given, we assume that all game states can eventually
         # reach an end in the game, except the two states where both players have no fingers available.
-        # this corresponds to an unreachable state vector of [0.5, 0, 0, ..., 0, 0.5, 0, ..., 0, 0]
-        # See self.getUnreachableStates for more information
-        if unreachableStates == None:
-            unreachableStates = [0] * self.vertexCount
-            unreachableStates[g.stateToStateIndex(((0, 0), (0, 0), 0))] = 0.5
-            unreachableStates[g.stateToStateIndex(((0, 0), (0, 0), 1))] = 0.5
-        mat = np.zeros((self.vertexCount, self.vertexCount))
-        solution = np.zeros(self.vertexCount)
+        # this corresponds to an indeterminate state vector of [0.5, 0, 0, ..., 0, 0.5, 0, ..., 0, 0]
+        # See self.getIndeterminateStates for more information
+        if indeterminateStates == None:
+            indeterminateStates = [0] * self.stateCount
+            indeterminateStates[self.stateToStateIndex(((0, 0), (0, 0), 0))] = 0.5
+            indeterminateStates[self.stateToStateIndex(((0, 0), (0, 0), 1))] = 0.5
+        mat = np.zeros((self.stateCount, self.stateCount))
+        solution = np.zeros(self.stateCount)
         # assign the rest of the states a value here:
-        for stateIndex in range(0, self.vertexCount):
-            if unreachableStates[stateIndex]:
+        for stateIndex in range(0, self.stateCount):
+            if indeterminateStates[stateIndex]:
                 mat[stateIndex][stateIndex] = 1
-                solution[stateIndex] = unreachableStates[stateIndex]
+                solution[stateIndex] = indeterminateStates[stateIndex]
             elif perfectPlaySolutions[stateIndex] != -1:
                 mat[stateIndex][stateIndex] = 1
                 solution[stateIndex] = perfectPlaySolutions[stateIndex]
@@ -281,34 +304,86 @@ class Graph:
                 bestState = child
         return bestState
 
-    def fairestStates(self, ranks):
-        ranksDict = {}
-        for i in range(0, self.vertexCount):
-            ranksDict.update({self.stateIndexToState(i) : abs(ranks[i] - 0.5)})
-        sortedDict = dict(sorted(ranksDict.items(), key=lambda item: item[1]))
-        for item in sortedDict:
-            print(f"{item} : {ranks[self.stateToStateIndex(item)]}")
+    def printStateOptions(self, state, ranks):
+        print(f"{state} : {ranks[self.stateToStateIndex(state)]}")
+        if(not len(self.children[self.stateToStateIndex(state)])):
+            print("GAME OVER, no moves can be made")
+        else:
+            print("Options are:")
+            for child in self.children[self.stateToStateIndex(state)]:
+                print(f"\t{self.stateIndexToState(child)} : {ranks[child]}")
+        print("\n")
 
-    def playGame(self, startState, ranks):
+    # Prints all the rankings of every state that
+    # isn't a guaranteed win/loss for a player
+    # and doesn't end in a draw
+    # i.e. states with rank != 1.0 and 
+    # states with rank != 0 and
+    # states with indeterminateStates of 0.0
+    # i.e. all states with a decimal rank != 0.5
+    # due to being indeterminate
+    def fairestStates(self, ranks, indeterminateStates):
+        fairestNode = None
+        for stateIndex in range(self.stateCount):
+            if not(indeterminateStates[stateIndex] or ranks[stateIndex] == 1 or ranks[stateIndex] == 0):
+                print(f"{self.stateIndexToState(stateIndex)} : {ranks[stateIndex]}")
+                if(fairestNode == None or abs(ranks[fairestNode]-0.5) > abs(ranks[stateIndex]-0.5)):
+                    fairestNode = stateIndex
+        print(f"The fairest state is: {CXBot.stateIndexToState(fairestNode)}: {ranks[fairestNode]}")
+    
+    def playAgainstSelf(self, startState, ranks):
         currState = startState
         visitedStates = []
         visitedStates.append(currState)
-        print(f"{self.stateIndexToState(currState)} : {ranks[currState]}")
         while(not (self.gameOverStatus(currState) == 0 or self.gameOverStatus(currState) == 1)):
+            debugFlag = True
+            if(debugFlag):
+                self.printStateOptions(self.stateIndexToState(currState), ranks)
+            else:
+                print(f"{self.stateIndexToState(currState)} : {ranks[currState]}")
             currState = self.chooseBestMove(ranks, currState)
-            print(f"{self.stateIndexToState(currState)} : {ranks[currState]}")
             if currState in visitedStates:
+                print(f"{self.stateIndexToState(currState)} : {ranks[currState]}")
+                print("We've been here before - we're going in circles!")
                 return
             else:
                 visitedStates.append(currState)
+        print(f"{self.stateIndexToState(currState)} : {ranks[currState]}")
+        print(f"GAME OVER - PLAYER {self.stateIndexToState(currState)[2] - 1} WINS!")
+
+    def playAgainstPlayer(self, startStateIndex, ranks):
+        currStateIndex = startStateIndex
+        done = False
+        while(not done):
+            if(self.gameOverStatus(currStateIndex) == 0 or self.gameOverStatus(currStateIndex) == 1):
+                print(f"GAME OVER - PLAYER {self.stateIndexToState(currStateIndex)[2] - 1} WINS!")
+                done = True
+            else:
+                for i in range(100):
+                    print()
+                print(f"The current state is {self.stateIndexToState(currStateIndex)}")
+                if(self.stateIndexToState(currStateIndex)[2]):
+                    for i in range(len(self.children[currStateIndex])):
+                        print(f"{i}: {self.stateIndexToState(self.children[currStateIndex][i])}")
+                    selected = input("Which state do you choose? Type \"exit\" to exit.\n")
+                    if(selected == "exit" or selected == "e"):
+                        done = True
+                    else:
+                        selected = int(selected)
+                        currStateIndex = self.children[currStateIndex][selected]
+                else:
+                    currStateIndex = self.chooseBestMove(ranks, currStateIndex)
+
 
 if __name__ == "__main__":
-    g = Graph(5)
-    pp = g.getSolutionsForPerfectPlay()
-    ur = g.getSolutionsForUnreachableStates()
-    ranks = g.solveForStateRankings(perfectPlaySolutions=pp, unreachableStates=ur)
-    g.fairestStates(ranks)
-    #startState = ((1, 1), (1, 1), 0)
-    #startState = g.stateToStateIndex(startState)
-    #g.playGame(startState, ranks)
+    CXBot = ChopstixBot(5, False)
+    perfectPlay = CXBot.getSolutionsForPerfectPlay()
+    indeterminateStates = CXBot.getSolutionsForIndeterminateStates()
+    ranks = CXBot.solveForStateRankings(perfectPlay, indeterminateStates)
     
+    #CXBot.fairestStates(ranks, ur)
+
+    startState = ((1, 2), (1, 1), 1)
+    startStateIndex = CXBot.stateToStateIndex(startState)
+    CXBot.playAgainstPlayer(startStateIndex, ranks)
+    #CXBot.playAgainstSelf(startStateIndex, ranks)
